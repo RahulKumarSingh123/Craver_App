5da89f651079a5c39a8e7d9f84d6915a9a781363
RahulKumarSingh123 <123647773+RahulKumarSingh123@users.noreply.github.com>
2024-09-07 19:01:09 +0530

    commit done

---
+++ components/BestSellerInfo.js
@@ -0,0 +1,36 @@
+export const BestSellerInfo = [
+    {
+        id:'1',
+        url: require('../assets/img/pizza/marg.jpeg'),
+        title: 'Margherita',
+        description: 'Classic delight with 100% real mozzarella cheese',
+        price: '₹214',
+        size: 'Regular | New Hand Tossed'
+    },
+    {
+        id:'2',
+        url: require('../assets/img/pizza/farmhouse.jpeg'),
+        title: 'Veggie Supreme',
+        description: 'Loaded with black olives, capsicum, onion, grilled mushroom, corn & tomato',
+        price: '₹325',
+        size: 'Regular | New Hand Tossed'
+    },
+    {
+        id:'3',
+        url: require('../assets/img/burger/bacon.jpeg'),
+        title: 'Chicken Dominator',
+        description: 'Loaded with double pepper barbecue chicken, peri-peri chicken, chicken tikka & grilled chicken rashers',
+        price: '₹499',
+        size: 'Large | Cheese Burst'
+      },
+      {
+        id:'4',
+        url: require('../assets/img/Chicken/tikka.jpeg'),
+
+        title: 'Barbeque Chicken',
+        description: 'Smoky BBQ chicken with extra cheese',
+        price: '₹429',
+        size: 'Large | Thin Crust'
+      },
+
+];
